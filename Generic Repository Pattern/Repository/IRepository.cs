4c2a2b4b6cf2336fe4d70432c232d8937f1baae0
angel-junior <iangeeel01@gmail.com>
2024-01-07 23:18:15 -0400

    generic repository pattern

---
+++ Generic Repository Pattern/Repository/IRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace Generic_Repository_Pattern.Repository
+{
+    public interface IRepository<T> where T : class
+    {
+
+        List<T> Get();
+        T GetById(int id);
+        T Save(T Data);
+
+
+    }
+}
